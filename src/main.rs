4e30f4c7de3b0715f45ff927e6c7800c2953adb9
Brian Schwind <brianmschwind@gmail.com>
2023-10-05 23:30:36 +0900

    Calculate parameters for desired PIO frequency

--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,85 @@
-use fugit::{HertzU32, RateExtU32};
+use fugit::{HertzU64, RateExtU64};
 
-const CRYSTAL_FREQUENCY_KHZ: u32 = 12_000; // 12 MHz
+const CRYSTAL_FREQUENCY_KHZ: u64 = 12_000; // 12 MHz
+
+const MIN_SYSTEM_CLOCK_HZ: u64 = 15_428_572;
+const MAX_SYSTEM_CLOCK_HZ: u64 = 133_000_000;
 
 // Voltage-Controlled Oscillator constants.
-const PICO_PLL_VCO_MIN_FREQ_KHZ: u32 = 750_000; // 750 MHz
-const PICO_PLL_VCO_MAX_FREQ_KHZ: u32 = 1_600_000; // 1600 MHz
+const PICO_PLL_VCO_MIN_FREQ_KHZ: u64 = 750_000; // 750 MHz
+const PICO_PLL_VCO_MAX_FREQ_KHZ: u64 = 1_600_000; // 1600 MHz
 
-const FEEDBACK_DIVIDER_MIN: u32 = 16;
-const FEEDBACK_DIVIDER_MAX: u32 = 320;
+const FEEDBACK_DIVIDER_MIN: u64 = 16;
+const FEEDBACK_DIVIDER_MAX: u64 = 320;
 
-const POST_DIVIDER_MIN: u32 = 1;
-const POST_DIVIDER_MAX: u32 = 7;
+const POST_DIVIDER_MIN: u64 = 1;
+const POST_DIVIDER_MAX: u64 = 7;
+
+const MAX_PIO_INT_DIVIDER: u64 = 65535;
 
 fn main() {
     println!("Using a {} crystal", CRYSTAL_FREQUENCY_KHZ.kHz::<1, 1>());
 
-    let desired_pio_frequency = 3_072_000;
+    let desired_pio_frequency_hz = 6_144_000u64;
 
-    let max_int_divider = 65535u32;
+    let nanosecond = 1_000_000_000;
 
-    for multiplier_increment in 1..=(max_int_divider * 256) {
-        let integer_divider = multiplier_increment / max_int_divider;
-        let frac_divider = multiplier_increment % max_int_divider;
+    let nanoseconds_per_cycle = nanosecond / desired_pio_frequency_hz;
+    println!("~{nanoseconds_per_cycle} nanoseconds per cycle");
 
-        // dbg!(integer_divider, frac_divider);
+    println!("Trying an integer divider first");
 
-        let desired_frequency = desired_pio_frequency * multiplier_increment;
+    // First try to find a whole integer divider that works for the desired PIO frequency.
+    // Start from the max divider and work down so we end up with a clock with less jitter.
+    for integer_multiplier in (1..=MAX_PIO_INT_DIVIDER).rev() {
+        let possible_system_clock_freq_hz = desired_pio_frequency_hz * integer_multiplier;
 
-        if desired_frequency % 256 != 0 {
-            continue;
-        }
-
-        let desired_frequency_khz = desired_frequency.Hz::<1, 1>().to_kHz();
-
-        let Some((vco_freq, post_div1, post_div_2)) = check_sys_clock(desired_frequency_khz.kHz()) else {
+        let Some((vco_freq, post_div1, post_div_2)) = check_sys_clock(possible_system_clock_freq_hz.Hz()) else {
             // println!("\tNo exact divider found");
             continue;
         };
 
-        println!("Desired frequency: {}MHz", desired_frequency_khz);
+        println!(
+            "Possible system clock: {} Hz",
+            possible_system_clock_freq_hz
+        );
 
-        println!("\tVCO Freq = {}kHz, Post Divider 1 = {}, Post Divider 2 = {}", vco_freq.to_kHz(), post_div1, post_div_2);
+        println!("\tVCO Freq = {}kHz, Post Divider 1 = {}, Post Divider 2 = {}, PIO divider = {}, PIO divider fraction = 0", vco_freq.to_kHz(), post_div1, post_div_2, integer_multiplier);
     }
 
-    // for desired_frequency_khz in 122_880_000..=122_880_001 {
-    //     println!("Desired frequency: {}kHz", desired_frequency_khz);
-    //     let Some((vco_freq, post_div1, post_div_2)) = check_sys_clock(desired_frequency_khz.kHz()) else {
-    //         println!("\tNo exact divider found");
-    //         continue;
-    //     };
+    println!();
+    println!("Now trying with fractional dividers");
 
-    //     println!("\tVCO Freq = {}kHz, Post Divider 1 = {}, Post Divider 2 = {}", vco_freq.to_kHz(), post_div1, post_div_2);
-    // }
+    for integer_multiplier in (1..=MAX_PIO_INT_DIVIDER).rev() {
+        for fraction in (0..256).rev() {
+            let numerator = 256 * integer_multiplier + fraction;
+            let possible_system_clock_freq_hz = (desired_pio_frequency_hz * numerator) / 256;
+
+            let Some((vco_freq, post_div1, post_div_2)) = check_sys_clock(possible_system_clock_freq_hz.Hz()) else {
+                // println!("\tNo exact divider found");
+                continue;
+            };
+
+            println!(
+                "Possible system clock: {} Hz",
+                possible_system_clock_freq_hz
+            );
+
+            println!("\tVCO Freq = {}kHz, Post Divider 1 = {}, Post Divider 2 = {}, PIO divider = {}, PIO divider fraction = {}", vco_freq.to_kHz(), post_div1, post_div_2, integer_multiplier, fraction);
+        }
+    }
 }
 
-// TODO(bschwind) - It seems lower jitter can be achieved by optimizing for higher frequencies
-//                  and larger dividers.
-pub fn check_sys_clock(desired_frequency: HertzU32) -> Option<(HertzU32, u8, u8)> {
+pub fn check_sys_clock(desired_frequency: HertzU64) -> Option<(HertzU64, u8, u8)> {
+    if desired_frequency.to_Hz() < MIN_SYSTEM_CLOCK_HZ
+        || desired_frequency.to_Hz() > MAX_SYSTEM_CLOCK_HZ
+    {
+        return None;
+    }
+
     let freq_khz = desired_frequency.to_kHz();
     let reference_divider = 1;
-    let reference_freq_khz: u32 = CRYSTAL_FREQUENCY_KHZ / reference_divider;
+    let reference_freq_khz: u64 = CRYSTAL_FREQUENCY_KHZ / reference_divider;
 
     for feedback_divide in (FEEDBACK_DIVIDER_MIN..=FEEDBACK_DIVIDER_MAX).rev() {
         let vco_khz = feedback_divide * reference_freq_khz;
@@ -85,8 +105,3 @@ pub fn check_sys_clock(desired_frequency: HertzU32) -> Option<(HertzU32, u8, u8)
 
     None
 }
-
-// Audio sample rate: 48_000
-// Bit clock rate: 48_000 * 64 = 3,072,000 Hz
-// Desired system clock: 40 - 122,880,000 Hz
-//                       39 - 119,808,000
